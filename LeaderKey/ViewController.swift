a2328dbe56e2c1e11e0131432cbb60346a842bfb
eightpigs <isme@lyinlong.me>
2018-01-01 14:02:27 +0800

    Initial Commit

---
+++ LeaderKey/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LeaderKey
+//
+//  Created by eightpigs on 01/01/2018.
+//  Copyright © 2018 刘印龙. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
